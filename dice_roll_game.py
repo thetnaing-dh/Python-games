1ba95d3ddfeba3354edb05d917d62c823fc14f7d
Thet Naing Oo <66229960+thetnaing-dh@users.noreply.github.com>
2025-09-05 12:41:12 +0630

    Update dice_roll_game.py

--- dice_roll_game.py
+++ dice_roll_game.py
@@ -1,16 +1,47 @@
 import random
+import time
+
+user_dices_sum = 0
+computer_dices_sum = 0
+
 #Loop
 while True:
     #Ask: roll the dice?
-    choice = input("Roll the dice? (y/n): ").lower()
+    choice = input("Roll the dices? (y/n): ").lower()
     #If user enters yes
     if choice =='y':
-    #   Generate two random numbers
+    #   Generate three random numbers
         dice1 = random.randint(1,6)
         dice2 = random.randint(1,6)
         dice3 = random.randint(1,6)
+    #   Sum User dices
+        user_dices_sum = dice1 + dice2 + dice3
     #   Print Item
-        print(f'({dice1}, {dice2}, {dice3})')
+        print(f'Your dices are :({dice1}, {dice2}, {dice3})')
+
+    # Computer Roll dices
+        symbols = ['.', '..', '...', '....', '.....']
+        i = 0
+        for i in range(5):
+            i = (i + 1) % len(symbols)
+            print('\r\033 [Computer roll the dices: %s' % symbols[i], flush=True, end='')
+            time.sleep(0.2)
+        dice1 = random.randint(1,6)
+        dice2 = random.randint(1,6)
+        dice3 = random.randint(1,6)
+    #   Sum Computer dices
+        computer_dices_sum = dice1 + dice2 + dice3
+    #   Print Item
+        print(f'\rComputer dices are :({dice1}, {dice2}, {dice3})')
+
+    #   Determine Winner
+        if user_dices_sum == computer_dices_sum:
+            print('Tie!')
+        elif user_dices_sum > computer_dices_sum:
+            print('You Win!')
+        else:
+            print('You Lose!')
+
     #If user enter no
     elif choice =='n':
     #   Print thank you message
